9e7e99f8f5256e6952d84b349b332cbb2857c6ab
shamspias <shamsuddinpias0@gmail.com>
2023-01-21 16:29:50 +0600

    feat: models

--- ausers/models.py
+++ ausers/models.py
@@ -1,3 +1,105 @@
+import uuid
+import re
 from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
+from rest_framework_simplejwt.tokens import RefreshToken
+from easy_thumbnails.fields import ThumbnailerImageField
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
+from easy_thumbnails.signals import saved_file
+from easy_thumbnails.signal_handlers import generate_aliases_global
 
-# Create your models here.
+from common.helpers import build_absolute_uri
+from notifications.services import notify, ACTIVITY_USER_RESETS_PASS
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import BaseUserManager
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    """
+    Handles password reset tokens
+    When a token is created, an e-mail needs to be sent to the user
+    """
+    reset_password_path = reverse('password_reset:reset-password-confirm')
+    context = {
+        'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
+        'reset_password_url': build_absolute_uri(f'{reset_password_path}?token={reset_password_token.key}'),
+    }
+
+    notify(ACTIVITY_USER_RESETS_PASS, context=context, email_to=[reset_password_token.user.email])
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(username=email, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        if extra_fields['sex']:
+            extra_fields.pop('sex')
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(_("phone number"), max_length=35, blank=True, null=True)
+    profile_picture = ThumbnailerImageField('ProfilePicture', upload_to='profile_pictures/', blank=True, null=True)
+    email = models.EmailField(_("email address"), unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def get_tokens(self):
+        refresh = RefreshToken.for_user(self)
+
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+    def __str__(self):
+        return self.username
+
+
+class Customer(models.Model):
+    """
+    Model for customers
+    """
+    CHOICE_SEX = (
+        ("male", "male"),
+        ("female", "female"),
+        ("other", "other"),
+    )
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(_("date of birth"), blank=True, null=True)
+    sex = models.CharField(_("sex"), max_length=10, blank=True, null=True, choices=CHOICE_SEX)
+
+    def __str__(self):
+        return self.user.email
+
+
+saved_file.connect(generate_aliases_global)
