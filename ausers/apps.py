0e7d77652aff5b2841d376703ecd560504576562
shamspias <shamsuddinpias0@gmail.com>
2023-01-21 16:55:23 +0600

    feat: signals auto creat profile

--- ausers/apps.py
+++ ausers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AusersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ausers'
+
+    def ready(self):
+        import ausers.signals
