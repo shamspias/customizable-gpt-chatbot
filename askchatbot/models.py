9901d84cc601e82a6bcc118b1062bed470a978d4
shamspias <shamsuddinpias0@gmail.com>
2023-01-21 20:03:06 +0600

    feat: conversation history model

--- askchatbot/models.py
+++ askchatbot/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ConversationHistory(models.Model):
+    """
+    To store the conversation history
+    """
+    conversation_id = models.CharField(max_length=100)
+    user_input = models.TextField()
+    chatbot_response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.conversation_id
