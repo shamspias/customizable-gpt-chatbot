d224a325bc1c2b41c7d58b6df51a18a1e2b84665
shamspias <shamsuddinpias0@gmail.com>
2023-01-21 20:03:20 +0600

    feat: add openai key

--- customizableGpt3Chatbot/settings/common.py
+++ customizableGpt3Chatbot/settings/common.py
@@ -326,3 +326,4 @@ SUMMERNOTE_CONFIG = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
