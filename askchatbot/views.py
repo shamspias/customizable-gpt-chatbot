059510899d2c94da2c88e9598435001e4319d0df
shamspias <shamsuddinpias0@gmail.com>
2023-01-21 20:22:51 +0600

    add conv history

--- askchatbot/views.py
+++ askchatbot/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import ConversationHistory
 from .tasks import chatbot_response
 
 
@@ -14,3 +15,8 @@ class ChatbotEndpoint(APIView):
         conversation_id = request.data.get('conversation_id')
         task = chatbot_response.apply_async(args=[user_input, conversation_id])
         return Response({"task_id": task.id})
+
+
+def get_conversation_history(conversation_id):
+    conversation_history = ConversationHistory.objects.filter(conversation_id=conversation_id)
+    return conversation_history
