d045fae449d0408fb90a6f5f8013f99d45bd17c8
shamspias <shamsuddinpias0@gmail.com>
2023-01-21 20:04:56 +0600

    feat: view chatbot

--- askchatbot/views.py
+++ askchatbot/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+import openai
+from .tasks import chatbot_response
 
-# Create your views here.
+
+class ChatbotView(View):
+    """
+    View for chatbot return task ID for celery
+    """
+
+    def post(self, request):
+        user_input = request.POST.get('user_input')
+        conversation_id = request.POST.get('conversation_id')
+        task = chatbot_response.apply_async(args=[user_input, conversation_id])
+        return JsonResponse({"task_id": task.id})
