6aa35d1c4d5eb511868e1406d21244cb4e525f99
shamspias <shamsuddinpias0@gmail.com>
2023-01-22 11:02:46 +0600

    refactor: celery config

--- customizableGpt3Chatbot/celery.py
+++ customizableGpt3Chatbot/celery.py
@@ -5,11 +5,11 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "customizableGpt3Chatbot.settings")
 
 app = Celery('config')
 
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # tasks can be added below
