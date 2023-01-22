3b920ad3aaedc3eb148e65f23fbe6e5eb1f91314
shamspias <shamsuddinpias0@gmail.com>
2023-01-22 13:15:18 +0600

    refactor: create history

--- askchatbot/views.py
+++ askchatbot/views.py
@@ -1,8 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 from .models import ConversationHistory
 from .tasks import chatbot_response
+
+
+def get_conversation_history(conversation_id):
+    """
+    Retrieve old conversations
+    """
+    if conversation_id:
+        conversation_history = ConversationHistory.objects.filter(conversation_id=conversation_id)
+    else:
+        conversation_history = None
+    return conversation_history
 
 
 class ChatbotEndpoint(APIView):
@@ -11,21 +22,35 @@ class ChatbotEndpoint(APIView):
     """
 
     def post(self, request):
+        """
+        Send the data to chatbot
+        """
         user_input = request.data.get('user_input')
-        conversation_id = request.data.get('conversation_id', 1)
         if user_input is None:
             return Response({"error": "No input values"})
-        task = chatbot_response.apply_async(args=[user_input, conversation_id])
+
+        # todo
+        # need to get last 10 or 20 conversation and pass to chatbot response
+        task = chatbot_response.apply_async(args=[user_input])
         return Response({"task_id": task.id})
 
     def get(self, request, format=None):
+        """
+        Retrieve the data from conversation history
+        """
         task_id = request.GET.get('task_id')
         if task_id is None:
             return Response({"error": "No Task ID"})
         response = chatbot_response.AsyncResult(task_id).get()
-        return Response({"data": response})
 
+        conversation_id = ConversationHistory.last_conversation_id
+        if conversation_id is None:
+            conversation_id = 0
+        else:
+            conversation_id += 1
 
-def get_conversation_history(conversation_id):
-    conversation_history = ConversationHistory.objects.filter(conversation_id=conversation_id)
-    return conversation_history
+        if response[0]:
+            conversation = ConversationHistory.objects.create(user=request.user, conversation_id=conversation_id,
+                                                              user_input=response[1], chatbot_response=response[0])
+            conversation.save()
+        return Response({"data": response[0]})
