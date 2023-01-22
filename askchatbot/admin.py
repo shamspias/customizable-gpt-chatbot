066b80212abc4c0c262c2110dac8fd0d1f35332a
shamspias <shamsuddinpias0@gmail.com>
2023-01-22 10:38:44 +0600

    feat: conversation history in admin

--- askchatbot/admin.py
+++ askchatbot/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import ConversationHistory
+
+
+@admin.register(ConversationHistory)
+class ConversationHistoryAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('conversation_id', 'user_input')}),
+        (
+            _('Personal info'),
+            {
+                'fields': (
+                    'conversation_id',
+                    'user_input',
+                    'chatbot_response',
+                    'timestamp',
+                )
+            },
+        ),
+        (_('Conversation ID'), {'fields': ('conversation_id',)}),
+        (_('User Input'), {'fields': ('user_input',)}),
+        (_('Response'), {'fields': ('chatbot_response', 'timestamp')}),
+    )
