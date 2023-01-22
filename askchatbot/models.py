a76930643e99496aae1bb24f835327b038d6b64f
shamspias <shamsuddinpias0@gmail.com>
2023-01-22 12:55:59 +0600

    feat: user conversation

--- askchatbot/models.py
+++ askchatbot/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from ausers.models import User
 
 
 class ConversationHistory(models.Model):
     """
     To store the conversation history
     """
-    conversation_id = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    conversation_id = models.CharField(max_length=100, unique=True)
     user_input = models.TextField(blank=True, null=True)
     chatbot_response = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
 
     def __str__(self):
         return self.conversation_id
