747c0650574a0017f0be11a8cc38e98b9ae43dc7
shamspias <shamsuddinpias0@gmail.com>
2023-01-22 10:44:05 +0600

    fix: admin ConversationHistoryAdmin

--- askchatbot/admin.py
+++ askchatbot/admin.py
@@ -1,26 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
 
 from .models import ConversationHistory
 
 
-@admin.register(ConversationHistory)
-class ConversationHistoryAdmin(UserAdmin):
-    fieldsets = (
-        (None, {'fields': ('conversation_id', 'user_input')}),
-        (
-            _('Personal info'),
-            {
-                'fields': (
-                    'conversation_id',
-                    'user_input',
-                    'chatbot_response',
-                    'timestamp',
-                )
-            },
-        ),
-        (_('Conversation ID'), {'fields': ('conversation_id',)}),
-        (_('User Input'), {'fields': ('user_input',)}),
-        (_('Response'), {'fields': ('chatbot_response', 'timestamp')}),
-    )
+class ConversationHistoryAdmin(admin.ModelAdmin):
+    list_display = ('conversation_id', 'user_input', 'chatbot_response', 'timestamp')
+    list_filter = ('conversation_id',)
+    search_fields = ('user_input', 'chatbot_response')
+
+
+admin.site.register(ConversationHistory, ConversationHistoryAdmin)
