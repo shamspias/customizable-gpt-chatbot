c3c60602f6e8b7acfb8b3116b1c343ac96cf0cbb
shamspias <shamsuddinpias0@gmail.com>
2023-01-22 12:36:21 +0600

    fix: null value

--- askchatbot/models.py
+++ askchatbot/models.py
@@ -6,8 +6,8 @@ class ConversationHistory(models.Model):
     To store the conversation history
     """
     conversation_id = models.CharField(max_length=100)
-    user_input = models.TextField()
-    chatbot_response = models.TextField()
+    user_input = models.TextField(blank=True, null=True)
+    chatbot_response = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
