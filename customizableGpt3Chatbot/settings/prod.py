e7b26d7510698863007117703f45f9d7fdf83143
shamspias <shamsuddinpias0@gmail.com>
2023-01-28 01:19:29 +0600

    feat: static url

--- customizableGpt3Chatbot/settings/prod.py
+++ customizableGpt3Chatbot/settings/prod.py
@@ -26,7 +26,7 @@ DATABASES = {
 # http://django-storages.readthedocs.org/en/latest/index.html
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIAFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# MEDIAFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY')
 AWS_S3_REGION_NAME = os.getenv('REGION_NAME')
@@ -37,8 +37,11 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = 'public-read'
 AWS_AUTO_CREATE_BUCKET = True
 AWS_QUERYSTRING_AUTH = False
-STATIC_URL = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
-MEDIA_URL = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
+# STATIC_ROOT = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
+# MEDIA_ROOT = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
 # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
