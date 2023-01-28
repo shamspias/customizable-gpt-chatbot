a787c7c53f739365a5a357c2ebfa6c16927d8816
shamspias <shamsuddinpias0@gmail.com>
2023-01-28 17:58:54 +0600

    fix: prompt text

--- askchatbot/tasks.py
+++ askchatbot/tasks.py
@@ -7,8 +7,6 @@ from .models import ConversationHistory
 @shared_task
 def chatbot_response(chatbot_prompt, conversation_id):
     openai.api_key = settings.OPEN_AI_KEY
-    # todo
-    # need to change the prompt with last 10 or 20 conversation target is under 500 token
     prompt = f"{chatbot_prompt}"
     completions = openai.Completion.create(
         engine="text-davinci-003",
