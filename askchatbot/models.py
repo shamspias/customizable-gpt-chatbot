e82c9ad73d3ac94bb9a86ae5a06a048e6980c86d
shamspias <shamsuddinpias0@gmail.com>
2023-01-28 13:20:27 +0600

    fix: model conversation

--- askchatbot/models.py
+++ askchatbot/models.py
@@ -7,7 +7,7 @@ class ConversationHistory(models.Model):
     To store the conversation history
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    conversation_id = models.CharField(max_length=100, unique=True)
+    conversation_id = models.PositiveIntegerField(default=0)
     user_input = models.TextField(blank=True, null=True)
     chatbot_response = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,7 +16,7 @@ class ConversationHistory(models.Model):
         ordering = ('-created_at',)
 
     def __str__(self):
-        return self.conversation_id
+        return self.user
 
     def last_conversation_id(self):
         """
