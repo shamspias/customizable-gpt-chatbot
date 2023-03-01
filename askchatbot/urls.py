e13ef81eb461a319133cf3733ca813dd4bfafe91
shamspias <shamsuddinpias0@gmail.com>
2023-03-01 20:03:26 +0600

    add url for basic

--- askchatbot/urls.py
+++ askchatbot/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
-from .views import ChatbotEndpoint, ConversationalHistory, DeleteConversationalHistory, SpeechToText
+from .views import (
+    ChatbotEndpoint,
+    ConversationalHistory,
+    DeleteConversationalHistory,
+    SpeechToText,
+    ChatbotBasicListCreateAPIView,
+    LanguageListCreateAPIView,
+    AdsListCreateAPIView,
+    ChatbotSuggestionsListCreateAPIView
+)
 
 urlpatterns = [
     path('', ChatbotEndpoint.as_view(), name='chatbot'),
     path('history/', ConversationalHistory.as_view(), name='conversations'),
     path('history/delete/', DeleteConversationalHistory.as_view(), name='conversations-delete'),
-    path('speech-to-text/', SpeechToText.as_view(), name='conversations-voice'),
-
+    # path('speech-to-text/', SpeechToText.as_view(), name='conversations-voice'),
+    path('chatbot-basics/', ChatbotBasicListCreateAPIView.as_view(), name='chatbot_basic_list_create'),
+    path('languages/', LanguageListCreateAPIView.as_view(), name='language_list_create'),
+    path('ads/', AdsListCreateAPIView.as_view(), name='ads_list_create'),
+    path('chatbot-suggestions/', ChatbotSuggestionsListCreateAPIView.as_view(), name='chatbot_suggestions_list_create'),
 ]
