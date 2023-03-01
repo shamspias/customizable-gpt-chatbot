bbe1381938937841cfa6c036915d05667f869e66
shamspias <shamsuddinpias0@gmail.com>
2023-03-01 20:07:16 +0600

    feat: admin

--- askchatbot/admin.py
+++ askchatbot/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ConversationHistory
+from .models import ConversationHistory, ChatbotBasic, Language, Ads, ChatbotSuggestions, ChatbotSuggestionsOptions
 
 
 class ConversationHistoryAdmin(admin.ModelAdmin):
@@ -10,6 +10,31 @@ class ConversationHistoryAdmin(admin.ModelAdmin):
     list_display = ('user', 'conversation_id', 'user_input', 'chatbot_response', 'created_at')
     list_filter = ('user', 'conversation_id',)
     search_fields = ('user_input', 'chatbot_response', 'user')
+
+
+@admin.register(ChatbotBasic)
+class ChatbotBasicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'logo', 'short_logo', 'back_ground')
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name')
+
+
+@admin.register(Ads)
+class AdsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'ads_image')
+
+
+@admin.register(ChatbotSuggestions)
+class ChatbotSuggestionsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(ChatbotSuggestionsOptions)
+class ChatbotSuggestionsOptionsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
 
 admin.site.register(ConversationHistory, ConversationHistoryAdmin)
