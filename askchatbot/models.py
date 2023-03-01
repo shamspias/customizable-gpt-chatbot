972d8561803015cc098ce7a2352af08c386fbb16
shamspias <shamsuddinpias0@gmail.com>
2023-03-01 19:59:07 +0600

    feat: customize

--- askchatbot/models.py
+++ askchatbot/models.py
@@ -27,3 +27,65 @@ class ConversationHistory(models.Model):
             return last_conversation.conversation_id
         except self.DoesNotExist:
             return None
+
+
+class ChatbotBasic(models.Model):
+    """
+    Model to set chatbot basic settings
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+    logo = models.FileField(upload_to="logo/", null=True)
+    short_logo = models.FileField(upload_to='short_logo/', null=True)
+    back_ground = models.FileField(upload_to='media/', null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    """
+    Language selection
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+    short_name = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ads(models.Model):
+    """
+    To manage Ads
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+    description = models.CharField(max_length=250, blank=True, null=True)
+    ads_image = models.FileField(upload_to='ads/', null=True)
+
+    class Meta:
+        verbose_name = "Ads"
+        verbose_name_plural = "Ads"
+
+    def __str__(self):
+        return self.name
+
+
+class ChatbotSuggestionsOptions(models.Model):
+    """
+    Information and suggestions Options
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+
+
+class ChatbotSuggestions(models.Model):
+    """
+    Information and suggestions
+    """
+    name = models.CharField(max_length=250, blank=True, null=True)
+    options = models.ManyToManyField(ChatbotSuggestionsOptions, related_name='chatbot')
+
+    class Meta:
+        verbose_name = "Chatbot Suggestion"
+        verbose_name_plural = "Chatbot Suggestions"
+
+    def __str__(self):
+        return self.name
