0d91043ae6365a9233e620407dce8cc6e447a042
shamspias <shamsuddinpias0@gmail.com>
2023-03-01 20:03:35 +0600

    feat: chatbot

--- askchatbot/serializers.py
+++ askchatbot/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ConversationHistory
+from .models import ConversationHistory, ChatbotBasic, Language, Ads, ChatbotSuggestions, ChatbotSuggestionsOptions
 
 
 class ConversationHistorySerializer(serializers.ModelSerializer):
@@ -10,3 +10,35 @@ class ConversationHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ConversationHistory
         fields = ('conversation_id', 'user_input', 'chatbot_response', 'timestamp')
+
+
+class ChatbotBasicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatbotBasic
+        fields = '__all__'
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = '__all__'
+
+
+class AdsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ads
+        fields = '__all__'
+
+
+class ChatbotSuggestionsOptionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatbotSuggestionsOptions
+        fields = '__all__'
+
+
+class ChatbotSuggestionsSerializer(serializers.ModelSerializer):
+    options = ChatbotSuggestionsOptionsSerializer(many=True)
+
+    class Meta:
+        model = ChatbotSuggestions
+        fields = '__all__'
