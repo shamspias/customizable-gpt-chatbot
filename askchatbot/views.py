10d303296fbf890392c4038ed3c7ff974f4ea847
shamspias <shamsuddinpias0@gmail.com>
2023-03-19 21:05:41 +0600

    feat: view

--- askchatbot/views.py
+++ askchatbot/views.py
@@ -1,111 +1,74 @@
-from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
-import speech_recognition as sr
-
-from .models import ConversationHistory
+from .models import Conversation, Message, FavoriteConversation
+from .serializers import ConversationSerializer, MessageSerializer, FavoriteConversationSerializer
 from .tasks import chatbot_response
 
 
-def get_conversation_history(conversation_id):
+class ConversationList(generics.ListCreateAPIView):
     """
-    Retrieve old conversations
+    This view allows listing all the conversations or creating a new one.
     """
-    if conversation_id:
-        conversation_history = ConversationHistory.objects.filter(conversation_id=conversation_id)
-    else:
-        conversation_history = None
-    return conversation_history
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
 
 
-class ConversationalHistory(APIView):
+class ConversationDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    APIView to get all the conversational history from a user
+    This view allows retrieving, updating, or deleting a conversation.
     """
-
-    def get(self, request):
-        conversations = ConversationHistory.objects.filter(user=request.user).order_by('-created_at')
-        user_conv = []
-        bot_conv = []
-        for conversation in conversations:
-            user_conv.append(conversation.user_input)
-            bot_conv.append(conversation.chatbot_response)
-        return Response({"botMsgArr": bot_conv, "userMsgArr": user_conv}, status=status.HTTP_200_OK)
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
 
 
-class ChatbotEndpoint(APIView):
+class MessageList(generics.ListCreateAPIView):
     """
-    APIView for chatbot return task ID for celery
+    This view allows listing all the messages in a conversation or creating a new one
+    with a user input and generating a response from GPT-3.
     """
+    serializer_class = MessageSerializer
 
-    def post(self, request):
-        """
-        Send the data to chatbot
-        example:
-        {
-            "user_input": "Hey there! how are you",
-            "language": "English"
-        }
-        """
-        user_input = request.data.get('user_input')
-        language = request.data.get('language', "English")
-        if user_input is None:
-            return Response({"error": "No input values"})
+    def get_queryset(self):
+        conversation = get_object_or_404(Conversation, pk=self.kwargs['pk'])
+        return Message.objects.filter(conversation=conversation)
 
-        # get last 15 conversation and pass to chatbot response
-        chatbot_prompt = ""
-        conversations = ConversationHistory.objects.filter(user=request.user).order_by('-created_at')[:15]
-        for conversation in conversations:
-            chatbot_prompt += "user:" + conversation.user_input + "\nbot:" + conversation.chatbot_response + "\n"
+    def perform_create(self, serializer):
+        conversation = get_object_or_404(Conversation, pk=self.kwargs['pk'])
+        user_input = serializer.validated_data['text']
+        language = serializer.validated_data['language']
 
-        chatbot_prompt += "user:" + user_input + "\nbot:"
-
-        # save the user input into database
-        try:
-            last_conversation = ConversationHistory.objects.filter(user=request.user).latest('conversation_id')
-            conversation_id = last_conversation.conversation_id
-            conversation_id += 1
-        except:
-            conversation_id = 0
-
-        if user_input:
-            conversation = ConversationHistory.objects.create(user=request.user, conversation_id=conversation_id,
-                                                              user_input=user_input)
-            conversation.save()
-
-        task = chatbot_response.apply_async(args=[chatbot_prompt, conversation_id, language])
-        return Response({"task_id": task.id}, status=status.HTTP_200_OK)
-
-    def get(self, request, format=None):
-        """
-        Retrieve the data from conversation history
-
-        example:
-        URL/chatbot?task_id=task_id
-
-        """
-        task_id = request.GET.get('task_id')
-        if task_id is None:
-            return Response({"error": "No Task ID"})
-
-        # return response from openAI and the user input as a List
-        response = chatbot_response.AsyncResult(task_id).get()
-
-        conversation_obj = ConversationHistory.objects.get(user=request.user, conversation_id=response[1])
-        conversation_obj.chatbot_response = response[0]
-        conversation_obj.save()
-
-        return Response({"data": response[0]}, status=status.HTTP_200_OK)
+        # Call GPT-3 API
+        gpt_output = chatbot_response(user_input, language)
+        serializer.save(conversation=conversation, is_user=True)
+        Message.objects.create(conversation=conversation, text=gpt_output, is_user=False)
 
 
-class SpeechToText(APIView):
+class FavoriteConversationList(generics.ListCreateAPIView):
     """
-    API View to Voice to Text
+    This view allows listing all the favorite conversations or adding a new one.
     """
-    def post(self, request):
-        recognizer = sr.Recognizer()
-        # audio_file = sr.AudioData(request.body, sample_rate=44100, sample_width=2, endpoint=sr.AudioFile.AudioData)
-        audio_file = sr.AudioData(request.body, sample_rate=44100, sample_width=2)
-        text = recognizer.recognize_google(audio_file)
+    queryset = FavoriteConversation.objects.all()
+    serializer_class = FavoriteConversationSerializer
 
-        return Response({'text': text}, status=status.HTTP_200_OK)
+
+class FavoriteConversationDetail(generics.RetrieveDestroyAPIView):
+    """
+    This view allows retrieving or deleting a favorite conversation.
+    """
+    queryset = FavoriteConversation.objects.all()
+    serializer_class = FavoriteConversationSerializer
+
+
+class ArchiveConversation(generics.UpdateAPIView):
+    """
+    This view allows archiving or unarchiving a conversation.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def patch(self, request, *args, **kwargs):
+        conversation = self.get_object()
+        conversation.is_archived = not conversation.is_archived
+        conversation.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
