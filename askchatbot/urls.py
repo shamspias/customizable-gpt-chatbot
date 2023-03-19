b5d1ee4c42e44bf7541e0856831cd5e291076c9f
shamspias <shamsuddinpias0@gmail.com>
2023-03-19 21:07:02 +0600

    feat: urls

--- askchatbot/urls.py
+++ askchatbot/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ChatbotEndpoint, ConversationalHistory, SpeechToText
+from . import views
 
 urlpatterns = [
-    path('', ChatbotEndpoint.as_view(), name='chatbot'),
-    path('history/', ConversationalHistory.as_view(), name='conversations'),
-    path('speech-to-text/', SpeechToText.as_view(), name='conversations-voice'),
-
+    path('conversations/', views.ConversationList.as_view(), name='conversation-list'),
+    path('conversations/<int:pk>/', views.ConversationDetail.as_view(), name='conversation-detail'),
+    path('conversations/<int:pk>/messages/', views.MessageList.as_view(), name='message-list'),
+    path('conversations/<int:pk>/archive/', views.ArchiveConversation.as_view(), name='archive-conversation'),
+    path('favorites/', views.FavoriteConversationList.as_view(), name='favorite-conversation-list'),
+    path('favorites/<int:pk>/', views.FavoriteConversationDetail.as_view(), name='favorite-conversation-detail'),
 ]
