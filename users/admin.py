4e0cbefe6a6b5f21a9b40c1ddf30d614df91b04b
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 02:27:47 +0600

    add user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("phone_number", "address", "profile_picture")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'address', 'profile_picture')
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
