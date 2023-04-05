73e2833d5579ae114a92b216eadbfc1ca65f4917
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 02:27:14 +0600

    add apps

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'chatbot.apps.ChatbotConfig',
+    'users.apps.UsersConfig',
+    'site_settings.apps.SiteSettingsConfig',
 ]
 
 MIDDLEWARE = [
