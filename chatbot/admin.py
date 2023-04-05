b83c39bf384fe845cd24f1e29ebbe5cf1d440993
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 02:49:19 +0600

    add admins

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Conversation, Message
 
-# Register your models here.
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    """
+    Admin site configuration for Conversation model.
+    """
+    list_display = ('id', 'user', 'created_at', 'updated_at', 'is_active', 'is_archived')
+    search_fields = ('user__username',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """
+    Admin site configuration for Message model.
+    """
+    list_display = ('id', 'conversation', 'content', 'created_at', 'is_from_user')
+    search_fields = ('conversation__id', 'content',)
+    list_filter = ('is_from_user',)
+    ordering = ('-created_at',)
