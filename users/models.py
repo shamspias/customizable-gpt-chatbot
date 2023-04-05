c9455e7d13926a7c5d31da9d5311c271da2b308e
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 02:27:25 +0600

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(_("Phone number"), max_length=15, blank=True, null=True)
+    address = models.TextField(_("Address"), blank=True, null=True)
+    profile_picture = models.ImageField(_("Profile picture"), upload_to="profile_pictures/", blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
