e6996887267f232d444726e48eaa427058149357
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 11:59:29 +0600

    fix: task

--- chatbot/tasks.py
+++ chatbot/tasks.py
@@ -1,5 +1,9 @@
 from celery import shared_task
+from celery.utils.log import get_task_logger
 import openai
+from django.core.exceptions import ObjectDoesNotExist
+
+logger = get_task_logger(__name__)
 
 from .models import Message
 
@@ -9,26 +13,30 @@ system_prompt = "This is a conversation with an AI assistant. The assistant is h
 
 
 @shared_task
-def send_gpt_request(conversation_id, message_list):
-    """
-    Task to send request to GPT-3.5 or upper and store the response as a message.
-    """
-    # Send request to GPT-3.5 (replace with actual GPT-3 API call)
-    gpt3_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                     {"role": "system",
-                      "content": system_prompt},
-                     {"role": "user", "content": "Who are you?"},
-                     {"role": "assistant",
-                      "content": "I am the sonic powered by ChatGpt.Contact me sonic@deadlyai.com"},
-                 ] + message_list
-    )
+def send_gpt3_request(conversation_id, message_list):
+    try:
+        # Send request to GPT-3 (replace with actual GPT-3 API call)
+        gpt3_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                         {"role": "system",
+                          "content": system_prompt},
+                         {"role": "user", "content": "Who are you?"},
+                         {"role": "assistant",
+                          "content": "I am the sonic powered by ChatGpt.Contact me sonic@deadlyai.com"},
+                     ] + message_list
+        )
 
-    # Store GPT response as a message
-    message = Message(
-        conversation_id=conversation_id,
-        content=gpt3_response["choices"][0]["message"]["content"].strip(),
-        is_from_user=False
-    )
-    message.save()
+    except Exception as e:
+        logger.error(f"Failed to send request to GPT-3: {e}")
+        return
+
+    try:
+        # Store GPT-3 response as a message
+        conversation = Conversation.objects.get(pk=conversation_id)
+        message = Message(conversation=conversation, content=gpt3_response, is_from_user=False)
+        message.save()
+    except ObjectDoesNotExist:
+        logger.error(f"Conversation with id {conversation_id} does not exist")
+    except Exception as e:
+        logger.error(f"Failed to save GPT-3 response as a message: {e}")
