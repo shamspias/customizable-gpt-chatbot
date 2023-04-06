976a16354882e953d921ce6af43f7ee782110843
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 16:54:41 +0600

    fix: user model

--- users/backends.py
+++ users/backends.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class EmailBackend(BaseBackend):
