6bcebc91126e6e7647eb40bc3868c7a50b536ee5
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 12:44:00 +0600

    fix: import

--- chatbot/views.py
+++ chatbot/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
-from django.db.models import Q
 
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
