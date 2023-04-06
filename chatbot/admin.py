237aea39381ba0fd730fba5b76e65517b1f255a8
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 12:11:22 +0600

    fix: admin conversation

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -7,7 +7,8 @@ class ConversationAdmin(admin.ModelAdmin):
     """
     Admin site configuration for Conversation model.
     """
-    list_display = ('id', 'user', 'created_at', 'updated_at', 'is_active', 'is_archived')
+    list_display = ('id', 'user', 'created_at', 'status')
+    list_filter = ('status', 'created_at')
     search_fields = ('user__username',)
 
 
@@ -17,6 +18,6 @@ class MessageAdmin(admin.ModelAdmin):
     Admin site configuration for Message model.
     """
     list_display = ('id', 'conversation', 'content', 'created_at', 'is_from_user')
-    search_fields = ('conversation__id', 'content',)
+    search_fields = ('user__username', 'content',)
     list_filter = ('is_from_user', 'user__username',)
     ordering = ('-created_at',)
