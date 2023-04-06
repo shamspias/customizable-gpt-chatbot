950e78c4bd3db2be4a4b45ac29ae1c99fd9ac6ee
shamspias <shamsuddinpias0@gmail.com>
2023-04-07 01:48:11 +0600

    update email backend

--- users/backends.py
+++ users/backends.py
@@ -1,24 +1,30 @@
-from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
-class EmailBackend(BaseBackend):
+class EmailBackend(ModelBackend):
     """
     Authenticate using an e-mail address.
     """
 
-    def authenticate(self, request, email=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
         try:
-            user = User.objects.get(email=email)
-            if user.check_password(password):
-                return user
-        except User.DoesNotExist:
-            return None
+            user = UserModel.objects.get(email=username)
+        except UserModel.DoesNotExist:
+            try:
+                user = UserModel.objects.get(username=username)
+            except UserModel.DoesNotExist:
+                return None
+
+        if user.check_password(password):
+            return user
 
     def get_user(self, user_id):
+        UserModel = get_user_model()
         try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
+            return UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
             return None
