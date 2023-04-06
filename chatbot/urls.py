364de854702420b347271fec9cf971684aff292d
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 12:24:00 +0600

    fixed: urls

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
-from .views import ConversationListCreate, ConversationDetail, MessageCreate, MessageList
+from . import views
 
 app_name = 'chatbot'
 
 urlpatterns = [
-    # Conversations list and create endpoint
-    path('conversations/', ConversationListCreate.as_view(), name='conversations-list-create'),
-
-    # Conversation detail, update, and delete endpoint
-    path('conversations/<int:pk>/', ConversationDetail.as_view(), name='conversation-detail'),
-
-    # Message create endpoint
-    path('conversations/<int:conversation_id>/messages/', MessageCreate.as_view(), name='message-create'),
-
-    # Last 10 messages list endpoint
-    path('conversations/<int:conversation_id>/messages/last10/', MessageList.as_view(), name='messages-last10'),
+    # List and create conversations
+    path('conversations/', views.ConversationListCreate.as_view(), name='conversation-list-create'),
+    # Retrieve, update, and delete a specific conversation
+    path('conversations/<int:pk>/', views.ConversationDetail.as_view(), name='conversation-detail'),
+    # Archive a conversation
+    path('conversations/<int:pk>/archive/', views.ConversationArchive.as_view(), name='conversation-archive'),
+    # End a conversation
+    path('conversations/<int:pk>/end/', views.ConversationEnd.as_view(), name='conversation-end'),
+    # Delete a conversation
+    path('conversations/<int:pk>/delete/', views.ConversationDelete.as_view(), name='conversation-delete'),
+    # List messages in a conversation
+    path('conversations/<int:conversation_id>/messages/', views.MessageList.as_view(), name='message-list'),
+    # Create a message in a conversation
+    path('conversations/<int:conversation_id>/messages/create/', views.MessageCreate.as_view(), name='message-create'),
 ]
