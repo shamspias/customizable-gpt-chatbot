88111fba711606d0dce7e25ac526d3ef03083df8
shamspias <shamsuddinpias0@gmail.com>
2023-04-07 00:22:01 +0600

    fixed: valade data

--- users/serializers.py
+++ users/serializers.py
@@ -23,7 +23,19 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'first_name', 'last_name']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data.pop('password')
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
 
         # Generate tokens for the user
         app = Application.objects.get(name=settings.APPLICATION_NAME)
