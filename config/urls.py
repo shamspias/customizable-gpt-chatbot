12daef2eda84e674a79a94cbee847cbe2cba1599
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 14:33:42 +0600

    fix: swagger root config

--- config/urls.py
+++ config/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     path('api/v1/chatbot/', include('chatbot.urls')),
 
     # Swagger URLs
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
