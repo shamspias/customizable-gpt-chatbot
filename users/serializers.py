dbd4b3cdf00f0bd4026feeea855057aef5be58c6
shamspias <shamsuddinpias0@gmail.com>
2023-04-07 00:25:11 +0600

    fixed: all field

--- users/serializers.py
+++ users/serializers.py
@@ -20,22 +20,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'first_name', 'last_name']
+        # fields = ['username', 'email', 'password', 'first_name', 'last_name']
+        fields = "__all__"
 
     def create(self, validated_data):
-        username = validated_data['username']
-        email = validated_data['email']
-        password = validated_data.pop('password')
-        first_name = validated_data['first_name']
-        last_name = validated_data['last_name']
-
-        user = User.objects.create_user(
-            username=username,
-            email=email,
-            password=password,
-            first_name=first_name,
-            last_name=last_name
-        )
+        user = User.objects.create_user(**validated_data)
 
         # Generate tokens for the user
         app = Application.objects.get(name=settings.APPLICATION_NAME)
