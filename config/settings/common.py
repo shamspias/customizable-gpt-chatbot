c3b58716196e95b997d176988baa897171dfa5bd
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 17:13:22 +0600

    fix: site id

--- config/settings/common.py
+++ config/settings/common.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-SITE_ID = os.getenv('SITE_ID', 1)
+SITE_ID = int(os.getenv('SITE_ID', 1))
 
 DEBUG = os.getenv('DJANGO_DEBUG', True)
 SITE_URL = os.getenv('SITE_URL', 'http://localhost:8000')
