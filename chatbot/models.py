71a3e90987b77dec2f54ccce1488664a78290349
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 12:09:56 +0600

    refactor: models

--- chatbot/models.py
+++ chatbot/models.py
@@ -6,11 +6,15 @@ class Conversation(models.Model):
     """
     Conversation model representing a chat conversation.
     """
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('archived', 'Archived'),
+        ('ended', 'Ended'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
-    is_archived = models.BooleanField(default=False)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
 
     def __str__(self):
         return f"Conversation {self.id} - {self.user.username}"
