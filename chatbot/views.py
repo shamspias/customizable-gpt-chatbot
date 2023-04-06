60786784f9cd0e34fe05285ff6c7f368e3bd1dd1
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 12:34:11 +0600

    update: views with conversation

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
+from django.db.models import Q
+
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from .tasks import send_gpt_request
@@ -9,15 +12,16 @@ from .tasks import send_gpt_request
 
 class LastMessagesPagination(LimitOffsetPagination):
     """
-    Pagination class to return the last messages.
+    Pagination class for last messages.
     """
     default_limit = 10
     max_limit = 10
 
 
+# List and create conversations
 class ConversationListCreate(generics.ListCreateAPIView):
     """
-    List all conversations or create a new one.
+    List and create conversations.
     """
     serializer_class = ConversationSerializer
 
@@ -28,9 +32,10 @@ class ConversationListCreate(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
+# Retrieve, update, and delete a specific conversation
 class ConversationDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update or delete a conversation instance.
+    Retrieve, update, and delete a specific conversation.
     """
     serializer_class = ConversationSerializer
 
@@ -44,9 +49,48 @@ class ConversationDetail(generics.RetrieveUpdateDestroyAPIView):
         return super().delete(request, *args, **kwargs)
 
 
+# Archive a conversation
+class ConversationArchive(APIView):
+    """
+    Archive a conversation.
+    """
+
+    def patch(self, request, pk):
+        conversation = get_object_or_404(Conversation, id=pk, user=request.user)
+        conversation.status = 'archived'
+        conversation.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# End a conversation
+class ConversationEnd(APIView):
+    """
+    End a conversation.
+    """
+
+    def patch(self, request, pk):
+        conversation = get_object_or_404(Conversation, id=pk, user=request.user)
+        conversation.status = 'ended'
+        conversation.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Delete a conversation
+class ConversationDelete(APIView):
+    """
+    Delete a conversation.
+    """
+
+    def delete(self, request, pk):
+        conversation = get_object_or_404(Conversation, id=pk, user=request.user)
+        conversation.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# List messages in a conversation
 class MessageList(generics.ListAPIView):
     """
-    List the last 10 messages of a conversation.
+    List messages in a conversation.
     """
     serializer_class = MessageSerializer
     pagination_class = LastMessagesPagination
@@ -56,9 +100,10 @@ class MessageList(generics.ListAPIView):
         return Message.objects.filter(conversation=conversation).select_related('conversation')
 
 
+# Create a message in a conversation
 class MessageCreate(generics.CreateAPIView):
     """
-    Create a new message.
+    Create a message in a conversation.
     """
     serializer_class = MessageSerializer
 
@@ -66,5 +111,17 @@ class MessageCreate(generics.CreateAPIView):
         conversation = get_object_or_404(Conversation, id=self.kwargs['conversation_id'], user=self.request.user)
         serializer.save(conversation=conversation, is_from_user=True)
 
+        # Retrieve the last 10 messages from the conversation
+        messages = Message.objects.filter(conversation=conversation).order_by('-created_at')[:10][::-1]
+
+        # Build the list of dictionaries containing the message data
+        message_list = [
+            {
+                "role": "user" if msg.is_from_user else "assistant",
+                "content": msg.content
+            }
+            for msg in messages
+        ]
+
         # Call the Celery task to get a response from GPT-3
-        send_gpt_request.delay(conversation.id, serializer.validated_data['content'])
+        send_gpt_request.delay(conversation.id, message_list)
