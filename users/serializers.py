87785a8b07bc444574540d5210ce8dc08cd8c1c4
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 16:25:01 +0600

    fix reg token

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from oauth2_provider.models import get_application_model
+from oauthlib.common import generate_token
+from oauth2_provider.settings import oauth2_settings
+from oauth2_provider.models import AccessToken, RefreshToken
+from django.conf import settings
+from django.utils import timezone
 
+Application = get_application_model()
 User = get_user_model()
 
 
@@ -16,7 +23,33 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        return user
+
+        # Generate tokens for the user
+        app = Application.objects.get(name=settings.APPLICATION_NAME)
+        access_token = generate_token()
+        refresh_token = generate_token()
+
+        AccessToken.objects.create(
+            user=user,
+            token=access_token,
+            application=app,
+            scope=oauth2_settings.DEFAULT_SCOPES,
+            expires=timezone.now() + oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS
+        )
+
+        RefreshToken.objects.create(
+            user=user,
+            token=refresh_token,
+            application=app,
+            access_token=AccessToken.objects.get(token=access_token)
+        )
+
+        tokens = {
+            'access_token': access_token,
+            'refresh_token': refresh_token
+        }
+
+        return tokens
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
