1234b566049b1755ed1270ae68572fde41264c60
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 12:12:43 +0600

    fix: serializers conversation

--- chatbot/serializers.py
+++ chatbot/serializers.py
@@ -2,22 +2,21 @@ from rest_framework import serializers
 from .models import Conversation, Message
 
 
+class ConversationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Conversation model.
+    """
+
+    class Meta:
+        model = Conversation
+        fields = ('id', 'created_at', 'status')
+
+
 class MessageSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Message model.
+    Serializer for Message model.
     """
 
     class Meta:
         model = Message
-        fields = ['content', 'created_at', 'is_from_user']
-
-
-class ConversationSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Conversation model.
-    """
-    messages = MessageSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Conversation
-        fields = ['id', 'created_at', 'updated_at', 'is_active', 'is_archived', 'messages']
+        fields = ('id', 'conversation', 'content', 'is_from_user', 'created_at')
