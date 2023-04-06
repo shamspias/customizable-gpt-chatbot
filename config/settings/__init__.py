eb14087ada9ae1879f07f564f09043199de8c1e5
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 14:08:41 +0600

    fix: import key

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 # DEV = False
 env_name = os.getenv('ENV_NAME', 'local')
-from key_values import *
+
+from .key_values import *
 
 if env_name == "prod":
     from .prod import *
