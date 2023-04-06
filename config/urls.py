b47581f3eeb438e61acbd7ae5f02adcec2cf334a
shamspias <shamsuddinpias0@gmail.com>
2023-04-06 16:05:40 +0600

    fix: urls with dashboard

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -39,3 +40,7 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+admin.site.site_title = settings.ADMIN_SITE_TITLE
+admin.site.index_title = settings.ADMIN_SITE_INDEX
