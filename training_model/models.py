5745c9bddf077980435f3f179fddb4b664bce14f
shamspias <shamsuddinpias0@gmail.com>
2023-04-10 16:20:15 +0600

    add models

--- training_model/models.py
+++ training_model/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
