60b34baf5faaee0475230667f292f84e0708a321
shamspias <shamsuddinpias0@gmail.com>
2023-04-10 14:16:00 +0600

    add model path

--- chatbot/tasks.py
+++ chatbot/tasks.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS as FISS
 from langchain.chat_models import ChatOpenAI
@@ -42,19 +43,46 @@ class FAISS(FISS):
             return pickle.load(f)
 
 
+def get_faiss_index(index_name):
+    faiss_obj_path = os.path.join(settings.BASE_DIR, "models", "{}.pickle".format(index_name))
+
+    if os.path.exists(faiss_obj_path):
+        # Load the FAISS object from disk
+        try:
+            faiss_index = FAISS.load(faiss_obj_path)
+            return faiss_index
+        except Exception as e:
+            logger.error(f"Failed to load FAISS index: {e}")
+            return None
+
+
 @shared_task
 def send_gpt_request(message_list):
     try:
-        # Add extra text to the content of the last message
-        information_source_text = " This is the extra text."
-        last_message = message_list[-1]
-        updated_content = last_message.content + information_source_text
+        # Load the FAISS index
+        faiss_index = get_faiss_index("buffer_salaries")
 
-        # Create a new HumanMessage object with the updated content
-        updated_message = HumanMessage(content=updated_content)
+        if faiss_index:
+            # Add extra text to the content of the last message
+            last_message = message_list[-1]
 
-        # Replace the last message in message_list with the updated message
-        message_list[-1] = updated_message
+            # Get the most similar documents to the last message
+            try:
+                docs = faiss_index.similarity_search(query=last_message.content, k=2)
+
+                updated_content = last_message.content + "\n\n"
+                for doc in docs:
+                    updated_content += doc.page_content + "\n\n"
+            except Exception as e:
+                logger.error(f"Failed to get similar documents: {e}")
+                updated_content = last_message.content
+
+            # Create a new HumanMessage object with the updated content
+            updated_message = HumanMessage(content=updated_content)
+
+            # Replace the last message in message_list with the updated message
+            message_list[-1] = updated_message
+
         messages = [
                        SystemMessage(content=system_prompt)
                    ] + message_list
