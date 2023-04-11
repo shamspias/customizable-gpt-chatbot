70abc436edbe577f62392944bfd2e7a024361390
shamspias <shamsuddinpias0@gmail.com>
2023-04-11 15:59:35 +0600

    refactor: avoid same file

--- training_model/models.py
+++ training_model/models.py
@@ -1,12 +1,18 @@
+import os
+import uuid
 from django.db import models
 
 
 def upload_to_faiss(instance, filename):
-    return 'documents/faiss/{0}'.format(filename)
+    basename, ext = os.path.splitext(filename)
+    new_filename = f"{basename}_{uuid.uuid4().hex}{ext}"
+    return f'documents/faiss/{new_filename}'
 
 
 def upload_to_pinecone(instance, filename):
-    return 'documents/pinecone/{0}'.format(filename)
+    basename, ext = os.path.splitext(filename)
+    new_filename = f"{basename}_{uuid.uuid4().hex}{ext}"
+    return f'documents/pinecone/{new_filename}'
 
 
 def dynamic_upload_to(instance, filename):
