dbb38f6674b3984a4f4a58a65dab2b89beaf8e2d
shamspias <shamsuddinpias0@gmail.com>
2023-04-11 13:38:03 +0600

    refactor: faiss helper with scalable

--- training_model/faiss_helpers.py
+++ training_model/faiss_helpers.py
@@ -1,20 +1,29 @@
 import os
 import pickle
 import mimetypes
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.document_loaders import UnstructuredWordDocumentLoader
-from langchain.document_loaders import PyPDFLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS as FISS
 from django.conf import settings
+from langchain.document_loaders import (
+    CSVLoader,
+    UnstructuredWordDocumentLoader,
+    PyPDFLoader,
+)
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS as BaseFAISS
 
-embeddings = OpenAIEmbeddings(openai_api_key=settings.OPENAI_API_KEY)
+OPENAI_API_KEY = settings.OPENAI_API_KEY
+BASE_DIR = settings.BASE_DIR
+MODELS_DIR = os.path.join(BASE_DIR, "models")
 
 
-class FAISS(FISS):
+class FAISS(BaseFAISS):
     """
     FAISS is a vector store that uses the FAISS library to store and search vectors.
     """
+
+    @classmethod
+    def load(cls, file_path):
+        with open(file_path, "rb") as f:
+            return pickle.load(f)
 
     def save(self, file_path):
         with open(file_path, "wb") as f:
@@ -28,37 +37,31 @@ class FAISS(FISS):
 def get_loader(file_path):
     mime_type, _ = mimetypes.guess_type(file_path)
 
-    if mime_type == 'application/pdf':
+    if mime_type == "application/pdf":
         return PyPDFLoader(file_path)
-    elif mime_type == 'text/csv':
+    elif mime_type == "text/csv":
         return CSVLoader(file_path)
-    elif mime_type in ['application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']:
+    elif mime_type in [
+        "application/msword",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    ]:
         return UnstructuredWordDocumentLoader(file_path)
     else:
         raise ValueError(f"Unsupported file type: {mime_type}")
 
 
 def build_or_update_faiss_index(file_path, index_name):
-    faiss_obj_path = os.path.join(settings.BASE_DIR, "models", "{}.pickle".format(index_name))
-
+    faiss_obj_path = os.path.join(MODELS_DIR, f"{index_name}.pickle")
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     loader = get_loader(file_path)
     pages = loader.load_and_split()
 
     if os.path.exists(faiss_obj_path):
-        # Load the FAISS object from disk
-        with open(faiss_obj_path, "rb") as f:
-            faiss_index = pickle.load(f)
-
-        # make new embeddings
+        faiss_index = FAISS.load(faiss_obj_path)
         new_embeddings = FAISS.from_documents(pages, embeddings, index_name=index_name)
-
-        # Add the new embeddings to the existing index
         faiss_index.add_vectors(new_embeddings)
-        faiss_index.save(faiss_obj_path)
     else:
-        # Build and save the FAISS object
-
         faiss_index = FAISS.from_documents(pages, embeddings, index_name=index_name)
-        faiss_index.save(faiss_obj_path)
 
+    faiss_index.save(faiss_obj_path)
     return faiss_index
