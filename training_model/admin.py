81fb9f2ed5ca970bd2b2345f4f23c802cc6b9b83
shamspias <shamsuddinpias0@gmail.com>
2023-04-11 20:10:03 +0600

    refactor admin

--- training_model/admin.py
+++ training_model/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from django.http import HttpResponseRedirect
-from django.urls import path, reverse
+from django.urls import reverse
 from django.utils.html import format_html
+
 from .models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
+    """
+    Admin View for Document
+    """
     list_display = ('id', 'index_name', 'file', 'storage_type', 'is_trained', 'uploaded_at', 'train_button')
     search_fields = ('file', 'index_name', 'storage_type')
-
-    change_form_template = 'admin/training_model/document/change_form.html'
 
     def train_button(self, obj):
         train_url = reverse('train_view', args=[obj.pk])
