a72ea990a2e9f838e3423a3283007c7806fe7b3e
shamspias <shamsuddinpias0@gmail.com>
2023-04-11 15:36:46 +0600

    fix: file

--- training_model/models.py
+++ training_model/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
 
+def upload_to_faiss(filename):
+    return 'documents/faiss/{0}'.format(filename)
+
+
+def upload_to_pinecone(filename):
+    return 'documents/pinecone/{0}'.format(filename)
+
+
 class Document(models.Model):
-    file = models.FileField(upload_to='documents/')
+    CHOICES = (
+        ('FAISS', 'FAISS'),
+        ('PINECONE', 'PINECONE')
+    )
+
+    file = models.FileField(upload_to=upload_to_pinecone)
     index_name = models.CharField(max_length=255)
+    storage_type = models.CharField(max_length=255, choices=CHOICES)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.storage_type == 'FAISS':
+            self.file.upload_to = upload_to_faiss
+        else:
+            self.file.upload_to = upload_to_pinecone
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.file.name
