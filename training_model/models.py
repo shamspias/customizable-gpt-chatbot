489927a83f369965c3bb88315a909c4fa687e753
shamspias <shamsuddinpias0@gmail.com>
2023-04-11 15:55:33 +0600

    refactor: file upload

--- training_model/models.py
+++ training_model/models.py
@@ -1,29 +1,31 @@
 from django.db import models
 
 
+def upload_to_faiss(instance, filename):
+    return 'documents/faiss/{0}'.format(filename)
+
+
+def upload_to_pinecone(instance, filename):
+    return 'documents/pinecone/{0}'.format(filename)
+
+
+def dynamic_upload_to(instance, filename):
+    if instance.storage_type == 'FAISS':
+        return upload_to_faiss(instance, filename)
+    else:
+        return upload_to_pinecone(instance, filename)
+
+
 class Document(models.Model):
     CHOICES = (
         ('FAISS', 'FAISS'),
         ('PINECONE', 'PINECONE')
     )
 
-    def upload_to_faiss(instance, filename):
-        return 'documents/faiss/{0}'.format(filename)
-
-    def upload_to_pinecone(instance, filename):
-        return 'documents/pinecone/{0}'.format(filename)
-
-    file = models.FileField(upload_to=upload_to_pinecone)
+    file = models.FileField(upload_to=dynamic_upload_to)
     index_name = models.CharField(max_length=255)
     storage_type = models.CharField(max_length=255, choices=CHOICES)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        if self.storage_type == 'FAISS':
-            self.file.upload_to = self.upload_to_faiss
-        else:
-            self.file.upload_to = self.upload_to_pinecone
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.file.name
