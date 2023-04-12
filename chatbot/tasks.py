9d2324c5e02dca57465a8092c2c05ed82041ff84
shamspias <shamsuddinpias0@gmail.com>
2023-04-12 22:27:34 +0600

    feat: title gen

--- chatbot/tasks.py
+++ chatbot/tasks.py
@@ -139,3 +139,23 @@ def send_gpt_request(message_list, name_space):
         logger.error(f"Failed to send request to GPT-3.5: {e}")
         return "Sorry, I'm having trouble understanding you."
     return assistant_response
+
+
+@shared_task
+def generate_title_request(message_list):
+    try:
+        openai.api_key = settings.OPENAI_API_KEY
+        # Send request to GPT-3 (replace with actual GPT-3 API call)
+        gpt3_response = openai.ChatCompletion.create(
+            model="davinci",
+            messages=[
+                         {"role": "system",
+                          "content": "Summarize and make a very short meaningful title based on that"},
+                     ] + message_list
+        )
+        response = gpt3_response["choices"][0]["message"]["content"].strip()
+
+    except Exception as e:
+        logger.error(f"Failed to send request to GPT-3.5: {e}")
+        return "Problematic title with error."
+    return response
