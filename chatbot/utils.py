d74bd5b60a1b89f819e5625aa066c64db0b418ac
shamspias <shamsuddinpias0@gmail.com>
2023-04-13 06:57:20 +0600

    feat: time since

---
+++ chatbot/utils.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+
+def time_since(dt):
+    """
+    Returns a string representing "time since" e.g.
+    """
+    now = datetime.now()
+    diff = now - dt
+
+    seconds = diff.total_seconds()
+    minutes = int(seconds // 60)
+    hours = int(minutes // 60)
+    days = int(hours // 24)
+    months = int(days // 30)
+    years = int(days // 365)
+    centuries = int(years // 100)
+
+    if centuries > 0:
+        return f"{centuries} century{'ies' if centuries > 1 else 'y'} ago"
+    elif years > 0:
+        return f"{years} year{'s' if years > 1 else ''} ago"
+    elif months > 0:
+        return f"{months} month{'s' if months > 1 else ''} ago"
+    elif days > 0:
+        return f"{days} day{'s' if days > 1 else ''} ago"
+    elif hours > 0:
+        return f"{hours} hour{'s' if hours > 1 else ''} ago"
+    elif minutes > 0:
+        return f"{minutes} minute{'s' if minutes > 1 else ''} ago"
+    else:
+        return f"{int(seconds)} second{'s' if seconds > 1 else ''} ago"
